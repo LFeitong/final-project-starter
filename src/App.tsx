2cfe8b641b430c28ce3984fa1f7ac922497543a0
Feitong Liu <feitong@udel.edu>
2021-10-07 12:17:23 -0400

    add welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,29 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+const welcomebutton=()=>{
+    console.log("hello");
+    alert("Welcome!");
+};
+
+
+
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+                <p onClick= {()=>welcomebutton()} >
+          Welcome!
                 </p>
+                
                 <a
                     className="App-link"
                     href="https://reactjs.org"
                     target="_blank"
                     rel="noopener noreferrer"
                 >
+                    
           Learn React
                 </a>
             </header>
